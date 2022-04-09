f2a409364d48bf55d96be074f9f8f83390171681
seonzoo <boy672820@gmail.com>
2022-04-09 18:05:52 +0900

    Add user module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { UserService } from './user.service';
-import { User as UserModel } from '@prisma/client';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(private readonly userService: UserService) {}
-
-  @Post('user')
-  async signupUser(
-    @Body() userData: { name?: string; email: string },
-  ): Promise<UserModel> {
-    return this.userService.createUser(userData);
-  }
-}
+export class AppController {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User as UserModel } from '@prisma/client';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string; password: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
